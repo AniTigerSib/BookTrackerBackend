fc802f8307787d2b8b2fa1f24d91749f3514536f
michael <khudoleev04@mail.ru>
2025-01-08 19:25:39 +0500

    main/Middlewares fixed, ORM models updated, gitignore updated.

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,7 @@
 import {Request, Response, Errback} from 'express';
+import { StatusCodes } from 'http-status-codes'
 
 export const errorHandlerMiddleware = async (err: Errback, req: Request, res: Response) => {
     console.log(err);
-    return res.status(500).json({ msg: "Something went wrong, try again later" });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, try again later" });
 }
--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,3 +1,4 @@
 import { Response, Request } from "express";
+import { StatusCodes } from 'http-status-codes'
 
-export const notFound = (_: Request, res: Response) => res.status(404).send("Route does not exist!");
+export const notFound = (_: Request, res: Response) => { res.status(StatusCodes.NOT_FOUND).send("Route does not exist!"); };
